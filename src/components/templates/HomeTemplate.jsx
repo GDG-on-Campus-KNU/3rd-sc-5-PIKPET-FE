9546b24e96c738cac0d97797cce23128f90147aa
joojjang <itchu999@naver.com>
2024-02-11 16:02:23 +0900

    [Feat] created WidgetArea

--- src/components/templates/HomeTemplate.jsx
+++ src/components/templates/HomeTemplate.jsx
@@ -11,26 +11,28 @@ import Container from "@components/molecules/Container";
 import { Img, ImgGroup } from "@styles/Img";
 import samplePicture from "@assets/sample-picture.png";
 import poster from "@assets/poster.png";
+import WidgetArea from "../organisms/WidgetArea";
 
 const HomeTemplate = () => {
   return (
     <ResponsiveLayout>
       <Header type="Default" />
-
       <ContentLayout>
         {/* <ButtonCTA type="SecondaryDisabled" title="입력하기" /> */}
         {/* <ButtonTag type="Default" title="#포메라니안"></ButtonTag> */}
         {/* <CTAContainer type="2ButtonUneven" title1="입력하기" title2="신청하기" /> */}
         {/* <Input placeholder="이름" /> */}
-        <Container title="내 검색 히스토리">
-          <ImgGroup>
-            <Img src={samplePicture} size="Small" />
-            <Img src={samplePicture} size="Small" />
-            <Img src={samplePicture} size="Small" />
-            <Img src={samplePicture} size="Small" />
-            <Img src={samplePicture} size="Small" />
-          </ImgGroup>
-        </Container>
+        <WidgetArea>
+          <Container title="내 검색 히스토리">
+            <ImgGroup>
+              <Img src={samplePicture} size="Small" />
+              <Img src={samplePicture} size="Small" />
+              <Img src={samplePicture} size="Small" />
+              <Img src={samplePicture} size="Small" />
+              <Img src={samplePicture} size="Small" />
+            </ImgGroup>
+          </Container>
+        </WidgetArea>
         <Img src={poster} width="100%" />
         <Footer />
         <NavBar />
