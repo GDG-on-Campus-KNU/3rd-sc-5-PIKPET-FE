6f0037cb8527138dedf296298c2e8ba26fab19c4
joojjang <itchu999@naver.com>
2024-02-18 13:45:51 +0900

    [Feat] made useTagsStore

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -9,7 +9,7 @@ export const useCurrentPageStore = create(
   "useCurrentPageStore"
 );
 
-// 검색어 관리
+// SearchBar에서 입력된 검색어들 관리
 export const useKeywordsStore = create(
   devtools((set) => ({
     keywords: "",
@@ -20,12 +20,55 @@ export const useKeywordsStore = create(
       const keywords = value.split(" "); // SearchBar에서 입력된 문자열을 공백으로 분리하여 키워드 배열 생성
       set({ keywordsList: keywords.filter(Boolean) }); // 빈 문자열 제외하고 키워드 목록 업데이트
     },
-    addKeyword: (value) => {
-      set({ keywordsList: [...keywordsList, value] }); // SearchFilter에서 클릭한 태그를 키워드 배열에 추가
-    },
     removeKeywordsList: () => set({ keywordsList: [] }),
   })),
   "useKeyworsStore"
+);
+
+// SearchFilter에서 클릭된 태그들 관리
+export const useTagsStore = create(
+  devtools((set) => ({
+    speciesTagsList: [],
+    addSpeciesTag: (value) => {
+      set({ speciesTagsList: [...speciesTagsList, value] }); // 클릭한 태그를 키워드 배열에 추가
+    },
+
+    breedTagsList: [],
+    addBreedTag: (value) => {
+      set({ breedTagsList: [...breedTagsList, value] });
+    },
+
+    minAge: 0,
+    setMinAge: (value) => {
+      set({ minAge: value });
+    },
+
+    maxAge: 0,
+    setMaxAge: (value) => {
+      set({ maxAge: value });
+    },
+
+    genderTagsList: [],
+    addGenderTag: (value) => {
+      set({ genderTagsList: [...genderTagsList, value] });
+    },
+
+    sizeTagsList: [],
+    addSizeTag: (value) => {
+      set({ sizeTagsList: [...sizeTagsList, value] });
+    },
+
+    colorTagsList: [],
+    addColorTag: (value) => {
+      set({ colorTagsList: [...colorTagsList, value] });
+    },
+
+    neutralized: true,
+    setNeutralized: (value) => {
+      set({ neutralized: value });
+    },
+  })),
+  "useTagsStore"
 );
 
 // 동물 정보 담은 리스트
