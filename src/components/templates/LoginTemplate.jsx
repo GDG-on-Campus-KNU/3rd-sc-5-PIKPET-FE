9401b75c30a9416ba20426a163bc48f2ffdcb89e
joojjang <itchu999@naver.com>
2024-02-19 18:46:35 +0900

    [Feat] set currentPage @LoginTemplate

--- src/components/templates/LoginTemplate.jsx
+++ src/components/templates/LoginTemplate.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useCurrentPageStore } from "@store";
+
 import Header from "@components/organisms/Header";
 import LoginForm from "@components/organisms/LoginForm";
 
@@ -5,6 +9,15 @@ import Layout, { Main, Contents } from "@styles/layout";
 import Text from "@styles/Text";
 
 const LoginTemplate = () => {
+  const navigate = useNavigate;
+  const { currentPage, setCurrentPage } = useCurrentPageStore();
+
+  // 최초 마운트시에(만) setCurrentPage
+  useEffect(() => {
+    setCurrentPage("/login");
+    localStorage.setItem("currentPage", JSON.stringify(currentPage)); // 로컬스토리지에 저장 (앱 리렌더링 시에도 값 보존 위해서)
+  }, []);
+
   return (
     <Layout backgroundColor="white">
       <Header type="Login" />
