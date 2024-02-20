d8323486e14d6301b3df5c8e86eea4c788e20922
joojjang <itchu999@naver.com>
2024-02-20 10:44:12 +0900

    [Chore] hide unused values @SearchFilter

--- src/components/organisms/SearchFilter.jsx
+++ src/components/organisms/SearchFilter.jsx
@@ -1,7 +1,7 @@
 import { useTagsStore } from "@store";
 
 import FilterItem from "@components/molecules/FilterItem";
-import Input, { AgeInput } from "@components/atoms/Input";
+import { AgeInput } from "@components/atoms/Input";
 import { ButtonFilterTag, ButtonTagGroup } from "@components/atoms/ButtonTag";
 import Toggle from "@components/atoms/Toggle";
 
@@ -9,29 +9,29 @@ import styled from "styled-components";
 import Text from "@styles/Text";
 
 const SearchFilter = () => {
-  const {
-    speciesTagsList,
-    addSpeciesTag,
-    removeSpeciesTag,
-    breedTagsList,
-    addBreedTag,
-    removeBreedTag,
-    minAge,
-    setMinAge,
-    maxAge,
-    setMaxAge,
-    genderTagsList,
-    addGenderTag,
-    removeGenderTag,
-    sizeTagsList,
-    addSizeTag,
-    removeSizeTag,
-    colorTagsList,
-    addColorTag,
-    removeColorTag,
-    neutralized,
-    setNeutralized,
-  } = useTagsStore();
+  // const {
+  //   speciesTagsList,
+  //   addSpeciesTag,
+  //   removeSpeciesTag,
+  //   breedTagsList,
+  //   addBreedTag,
+  //   removeBreedTag,
+  //   minAge,
+  //   setMinAge,
+  //   maxAge,
+  //   setMaxAge,
+  //   genderTagsList,
+  //   addGenderTag,
+  //   removeGenderTag,
+  //   sizeTagsList,
+  //   addSizeTag,
+  //   removeSizeTag,
+  //   colorTagsList,
+  //   addColorTag,
+  //   removeColorTag,
+  //   neutralized,
+  //   setNeutralized,
+  // } = useTagsStore();
 
   return (
     <StyledSearchFilter>
