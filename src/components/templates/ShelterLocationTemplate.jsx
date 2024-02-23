8921169430f6d093ffa0b526f903ba2eefc1d7b2
joojjang <itchu999@naver.com>
2024-02-23 21:04:28 +0900

    [Feat] set lat & lng dynamically @ShelterLocationT

--- src/components/templates/ShelterLocationTemplate.jsx
+++ src/components/templates/ShelterLocationTemplate.jsx
@@ -24,19 +24,12 @@ const ShelterLocationTemplate = () => {
   }, [currentPage]);
 
   // 지도에 핀 찍을 위도와 경도 ======================================
-  // const shelter = petInfoDetail.shelter;
-  // 임시
-  const shelter = {
-    branchName: "Huel-Ratke",
-    contact: "433-830-2032",
-    id: 13,
-    location: null,
-    latitude: 13.9142931,
-    longitude: -87.921223,
-  };
-  const lat = shelter.latitude;
-  const lng = shelter.longitude;
-  const shelterName = shelter.branchName;
+  // 객체의 기본값 설정해주기
+  // url 바로 찍으면 저장된 값 없어서 기본값으로 할당됩니딧...
+  const shelter = petInfoDetail.shelter || {};
+  const lat = shelter.latitude || 35.888841470939546;
+  const lng = shelter.longitude || 128.61024856567383;
+  const shelterName = shelter.branchName || "";
 
   return (
     <Layout backgroundColor="white">
