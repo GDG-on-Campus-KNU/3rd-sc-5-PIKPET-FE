67cb30602ce261269b4fefcada9bcc1b0d8e199a
joojjang <itchu999@naver.com>
2024-02-24 19:33:34 +0900

    [Feat] create shelter contact modal

--- src/components/templates/PetInfoDetailTemplate.jsx
+++ src/components/templates/PetInfoDetailTemplate.jsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
-import { useCurrentPageStore, usePetInfoDetailStore } from "@store";
+import { useCurrentPageStore, usePetInfoDetailStore, useModalStore } from "@store";
 import axios from "axios";
 
 import Header from "@components/organisms/Header";
 import PetInfoDetail from "@components/organisms/PetInfoDetail";
 import CTAContainer from "@components/molecules/CTAContainer";
+import ModalContact from "@components/atoms/ModalContact";
 
+import styled from "styled-components";
 import Text from "@styles/Text";
 import Layout, { Main, Contents } from "@styles/Layout";
 
@@ -16,6 +18,7 @@ const PetInfoDetailTemplate = () => {
   const petId = params.petId; // useParams를 통해 URL에서 가져온 petId
   const { currentPage, setCurrentPage } = useCurrentPageStore();
   const { petInfoDetail, setPetInfoDetail } = usePetInfoDetailStore();
+  const { isModalOpen, openModal, closeModal } = useModalStore();
 
   // 로컬 스토리지 값 관리: 앱 리렌더링 시에도 값 보존 위함 ----------
   // 최초 마운트시에(만) 실행
@@ -79,8 +82,11 @@ const PetInfoDetailTemplate = () => {
   }, []);
 
   // show shelter contact info ============================
-  const handleContact = () => {
-    console.log("contact: ", petInfoDetail.shelter.contact);
+
+  const handleClickContact = () => {
+    // console.log("contact: ", petInfoDetail.shelter.contact); // for test
+    openModal();
+    // setIsDarkBackground(true); // 모달 열렸을 시 배경색 어둡게
   };
 
   // move to Application page =============================
@@ -89,24 +95,39 @@ const PetInfoDetailTemplate = () => {
   };
 
   return (
-    <Layout backgroundColor="white">
-      <Header type="PetInfoDetail" />
+    <>
+      <Layout backgroundColor="white">
+        <Header type="PetInfoDetail" />
 
-      <Main>
-        <Contents>
-          <PetInfoDetail />
-        </Contents>
+        <Main>
+          <Contents>
+            <PetInfoDetail />
+          </Contents>
 
-        <CTAContainer
-          type="2ButtonEven"
-          title1="Contact"
-          title2="Apply"
-          onClick1={handleContact}
-          onClick2={handleApply}
-        />
-      </Main>
-    </Layout>
+          <CTAContainer
+            type="2ButtonEven"
+            title1="Contact"
+            title2="Apply"
+            onClick1={handleClickContact}
+            onClick2={handleApply}
+          />
+        </Main>
+      </Layout>
+      {isModalOpen && <ModalContact />}
+      {isModalOpen && <DarkBackgroundLayer />}
+    </>
   );
 };
+
+const DarkBackgroundLayer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 999; /* 모달보다 아래에 위치하도록 z-index 조절 */
+  pointer-events: auto; /* 뒤에 깔린 것들 클릭 방지 */
+`;
 
 export default PetInfoDetailTemplate;
